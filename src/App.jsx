7661307fbd5dcf7d39c3c1139d0f8f0a2db6ec63
Mindula <mindula@gmail.com>
2026-08-19 12:09:20 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css'
+
+function App() {
+
+  const trackEvent = (eventName, eventData = {}) => {
+    if (window.gtag) {
+      window.gtag('event', eventName, eventData)
+      console.log('GA4 Event:', eventName, eventData)
+    } else {
+      console.log('gtag is not loaded')
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1>GA4 Testing Website</h1>
+
+      <p>
+        My GA4 Corner
+      </p>
+
+      <div className="card">
+        <h2>Test Events</h2>
+
+        <button
+          onClick={() =>
+            trackEvent('button_click', {
+              button_name: 'Test Button'
+            })
+          }
+        >
+          Test Button Click
+        </button>
+
+        <button
+          onClick={() =>
+            trackEvent('signup_click', {
+              method: 'test'
+            })
+          }
+        >
+          Test Sign Up
+        </button>
+
+        <button
+          onClick={() =>
+            trackEvent('purchase_test', {
+              item_name: 'Test Product',
+              value: 10,
+              currency: 'USD'
+            })
+          }
+        >
+          Test Purchase Event
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
